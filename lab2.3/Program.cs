f9db89dbc18305277a181f6b53dc7de0b7d8f986
glebe <glebe@BELG04>
2022-10-12 21:14:49 +0200

    Добавьте файлы проекта.

---
+++ lab2.3/Program.cs
@@ -0,0 +1,22 @@
+﻿// Лукьянов Глеб 22-ИСП-2\1 Вариант:2 Уровень:Высокий 
+Console.Write("Введите скорость ветра в м/с");
+int speed = int.Parse(Console.ReadLine());
+
+if (speed <= 4)
+{
+    Console.WriteLine("Уровень ветра - слабый");
+
+}
+else if (speed <= 10)
+{
+    Console.WriteLine("Уровень ветра - умеренный");
+
+}
+else if (speed <= 11)
+{
+    Console.WriteLine("Уровень ветра - сильный");
+}
+else
+{
+    Console.WriteLine("Уровень ветра - ураган");
+}
